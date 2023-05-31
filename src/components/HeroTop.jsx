a8b04fd7c7a8a2898fc2a1fe7513826143d98eb4
dimysenpai <dimitrymabom@gmail.com>
2023-05-31 14:20:06 +0100

    finish structure topbar, create _placeholder.scss

---
+++ src/components/HeroTop.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeroTop = () => {
+  return <div>HeroTop</div>;
+};
+
+export default HeroTop;
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,27 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TopBar = () => {
+  const [colorSvg, setColorSvg] = useState("#fff");
+
   return (
     <header className="topbar">
       <div className="top">
-        <span className="logotext">Gic Bonus</span>
+        <div className="logotext">Gic Bonus</div>
         <div className="search-container">
           <input type="search" value="#" />
         </div>
         <div className="actions">
-          <span className="notification">
-            <img src="./ico/Outline/bell.svg" alt="bell icon" />
-            <span>2</span>
-          </span>
-          <span className="panier">
-            <img src="./ico/Outline/shopping-bag.svg" alt="bell icon" />
-            <span>2</span>
-          </span>
-          <div className="profil-container">
-            <img className="icon-profil" src="./senpai.png" alt="#" />
-            <img src="./ico/Outline/chevron-down.svg" alt="bell icon" />
-            {/* <div className="profil-menu">azerrt</div> */}
+          <div className="notification">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
+                stroke={colorSvg}
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            <div>2</div>
           </div>
+          <div className="panier">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16 11V7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7V11M5 9H19L20 21H4L5 9Z"
+                stroke={colorSvg}
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+
+            <div>2</div>
+          </div>
+          {/* <div className="profil-container">
+            <img className="icon-profil" src="./senpai.png" alt="#" />
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19 9L12 16L5 9"
+                stroke={colorSvg}
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            <div className="profil-menu">azerrt</div>
+          </div> */}
         </div>
       </div>
       <div className="bottom">

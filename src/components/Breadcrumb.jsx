e618f9c80cfe5a98d3c2d593e64a61c31be782b6
dimysenpai <dimitrymabom@gmail.com>
2023-06-21 19:27:33 +0100

    file

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Breadcrumb = () => {
-  return <div className="breadcrumb">Boutique</div>;
+const BreadCrumb = () => {
+  return (
+    <div className="breadcrumb">
+      <span className="not_current">
+        <a href="#">Boutique</a>
+      </span>
+      <span className="separator"> / </span>
+    </div>
+  );
 };
 
-export default Breadcrumb;
+export default BreadCrumb;

3f99b3f17af51ab1f9295f1362ed00d41c4b5d6a
dimysenpai <dimitrymabom@gmail.com>
2023-07-11 17:56:35 +0100

    delete card

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Breadcrumb from "../components/Breadcrumb";
 import Sort from "../components/Sort";
-import Card from "../components/card";
 
 const Store = () => {
   return (

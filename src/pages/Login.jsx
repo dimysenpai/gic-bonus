7448849d583af22f155ca226c7f2c738e5b43167
dimysenpai <dimitrymabom@gmail.com>
2023-07-11 16:15:35 +0100

    config de login avec js-cookie

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,68 @@
-import React from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    let modalAlert = document.querySelector(".modal-alert");
+
+    axios({
+      method: "post",
+      url: `${import.meta.env.VITE_APP_API_URL}user/login`,
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: new URLSearchParams({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((response) => {
+        if (response.data.status === "success") {
+          Cookies.set("id_user", response.data.user.id_user);
+          Cookies.set("firstname", response.data.user.firstname);
+          Cookies.set("lastname", response.data.user.lastname);
+          Cookies.set("phone", response.data.user.phone);
+          Cookies.set("role", response.data.user.role);
+
+          window.location = "/";
+        }
+        if (response.data.status === "error") {
+          modalAlert.innerText = response.data.message;
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="login">
+      <div className="modal-alert"></div>
       <div className="login-container">
         <div className="welcome">
           <img src="./images/gic-bonus.png" alt="" />
           <h2>Gic Bonus</h2>
           <p>Bienvenue dans l'aventure gic bonus</p>
         </div>
-        <form method="Post" className="login-form">
+        <form onSubmit={handleLogin} id="login-form" className="login-form">
           <h1>Connectez-Vous</h1>
 
           <div className="field">
             <label htmlFor="email">Email</label>
             <input
-              type="email"
+              type="text"
               id="email"
               name="email"
               placeholder="Votre Email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
           </div>
 
@@ -30,6 +73,8 @@ const Login = () => {
               id="password"
               name="password"
               placeholder="Votre Mot de passe"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
             />
           </div>
 
@@ -45,7 +90,9 @@ const Login = () => {
             <Link to={"/signin"}>Je n'ai pas encore de Compte !</Link>
           </div>
 
-          <button className="login-submit">Connexion</button>
+          <button type="submit" className="login-submit">
+            Connexion
+          </button>
         </form>
       </div>
     </div>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,16 +1,61 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignIn = () => {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [phone, setPhone] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignin = (e) => {
+    e.preventDefault();
+    let modalAlert = document.querySelector(".modal-alert");
+
+    axios({
+      method: "post",
+      url: `${import.meta.env.VITE_APP_API_URL}user/create`,
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: new URLSearchParams({
+        firstname: firstname,
+        lastname: lastname,
+        phone: phone,
+        username: username,
+        email: email,
+        password: password,
+      }),
+    })
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.status === "success") {
+          console.log(response.data.message);
+          // window.location = "/";
+        }
+        if (response.data.status === "error") {
+          modalAlert.innerText = response.data.message;
+          console.log(response.data.message);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="signIn">
+      <div className="modal-alert"></div>
       <div className="signIn-container">
         <div className="welcome">
           <img src="./images/gic-bonus.png" alt="" />
           <h2>Gic Bonus</h2>
           <p>Bienvenue dans l'aventure gic bonus</p>
         </div>
-        <form method="Post" className="signIn-form">
+        <form onSubmit={handleSignin} id="signIn-form" className="signIn-form">
           <h1>Connectez-Vous</h1>
 
           <div className="field">
@@ -20,6 +65,8 @@ const SignIn = () => {
               id="firstname"
               name="firstname"
               placeholder="Votre Nom"
+              onChange={(e) => setFirstname(e.target.value)}
+              value={firstname}
             />
           </div>
 
@@ -30,6 +77,8 @@ const SignIn = () => {
               id="lastname"
               name="lastname"
               placeholder="Votre Prenom"
+              onChange={(e) => setLastname(e.target.value)}
+              value={lastname}
             />
           </div>
 
@@ -40,16 +89,20 @@ const SignIn = () => {
               id="phone"
               name="phone"
               placeholder="Votre Téléphone"
+              onChange={(e) => setPhone(e.target.value)}
+              value={phone}
             />
           </div>
 
           <div className="field">
-            <label htmlFor="pseudo">Nom d'utilisateur</label>
+            <label htmlFor="username">Nom d'utilisateur</label>
             <input
               type="text"
-              id="pseudo"
-              name="pseudo"
+              id="username"
+              name="username"
               placeholder="Votre Nom d'utilisateur"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
             />
           </div>
 
@@ -60,6 +113,8 @@ const SignIn = () => {
               id="email"
               name="email"
               placeholder="Votre Email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
           </div>
 
@@ -70,6 +125,8 @@ const SignIn = () => {
               id="password"
               name="password"
               placeholder="Votre Mot de passe"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
             />
           </div>
 
@@ -85,7 +142,9 @@ const SignIn = () => {
             <Link to={"/login"}>J'ai déjà un Compte !</Link>
           </div>
 
-          <button className="signIn-submit">S'inscrire</button>
+          <button type="submit" className="signIn-submit">
+            S'inscrire
+          </button>
         </form>
       </div>
     </div>
